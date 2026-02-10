fd93b34f9810bfd93397ad837b7cfc332f16bbe5
Stella Kim <skim28@csu.fullerton.edu>
2026-02-10 10:43:25 -0800

    Update Recipe_TinderApp.swift
    with authentication

--- Recipe_Tinder/Recipe_Tinder/App/Recipe_TinderApp.swift
+++ Recipe_Tinder/Recipe_Tinder/App/Recipe_TinderApp.swift
@@ -3,15 +3,19 @@
 //  Recipe_Tinder
 //
 //  Created by Sebastian C on 1/27/26.
-//
+//    Edited by Stella K on 2/10/26
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct Recipe_TinderApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
-            MainTabView()
+            AuthenticationRootView()
         }
     }
 }
