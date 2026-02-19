4915f90092bc3c310383de422f364033c7b2af89
Stella Kim <skim28@csu.fullerton.edu>
2026-02-19 10:36:49 -0800

    Update Recipe_TinderApp.swift
    With Firebase Authentication

--- Recipe_Tinder/Recipe_Tinder/App/Recipe_TinderApp.swift
+++ Recipe_Tinder/Recipe_Tinder/App/Recipe_TinderApp.swift
@@ -4,15 +4,23 @@
 //
 //  Created by Sebastian C on 1/27/26.
 //    Edited by Stella K on 2/10/26
+//   Updated by Stella K on 2/19/26
 
 import SwiftUI
 import FirebaseCore
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
 @main
 struct Recipe_TinderApp: App {
-    init() {
-        FirebaseApp.configure()
-    }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             AuthenticationRootView()
